1fd190cdeff18a9b3925d5c5d0422f51bb2103eb
dherrada <dylan.herrada@adafruit.com>
2021-06-03 15:06:11 -0400

    Made use dherrada

--- bundle.sh
+++ bundle.sh
@@ -22,7 +22,7 @@ while test $# -gt 0; do
 done
 echo $OUTPUT
 mkdir $OUTPUT/bundle
-curl -u dherrada:$GH_REPO_TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/adafruit/Adafruit_CircuitPython_Bundle/releases/latest | jq '.["assets"][].browser_download_url' | jq 'if . | test("6.x-mpy") == true then . else empty end' | xargs wget -O $OUTPUT/bundle-6.x.zip
+curl -u $USER:$GH_REPO_TOKEN -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/adafruit/Adafruit_CircuitPython_Bundle/releases/latest | jq '.["assets"][].browser_download_url' | jq 'if . | test("6.x-mpy") == true then . else empty end' | xargs wget -O $OUTPUT/bundle-6.x.zip
 rm -rf $OUTPUT/adafruit-circuitpython-bundle-6*
 unzip $OUTPUT/bundle-6.x -d $OUTPUT
 rm $OUTPUT/bundle-6.x.zip
